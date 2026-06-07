50a9c5221751a0997ad0a563093504dc7f144a63
vinaykumar733 <75089114+vinaykumar733@users.noreply.github.com>
2026-06-07 16:29:26 +0530

    Delete pg-buz directory

--- pg-buz/prisma/migrations/20260602122436_add_chintu_leads/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ChintuLead" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "phone" TEXT NOT NULL,
-    "requirement" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
